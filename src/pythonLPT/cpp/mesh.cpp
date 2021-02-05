64d3350cccc87b764b6c71de9f31f8ba697517a4
Georgia Stuart <georgia.stuart@gmail.com>
2021-02-04 18:36:02 -0600

    Add mesh files

---
+++ src/pythonLPT/cpp/mesh.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Georgia Stuart on 2/4/21.
+//
+
+#include "mesh.h"
---
+++ src/pythonLPT/cpp/mesh.h
@@ -0,0 +1,18 @@
+//
+// Created by Georgia Stuart on 2/4/21.
+//
+
+#ifndef LPTCPP_MESH_H
+#define LPTCPP_MESH_H
+
+#include "coordinate.h"
+
+class MeshNode {
+public:
+    double water_density, temperature;
+    CoodinateD velocity, location;
+};
+
+class Mesh {};
+
+#endif //LPTCPP_MESH_H
