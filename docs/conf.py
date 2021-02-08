9a6092c870783ab1e245db6a0b5a710b082b0b0a
Georgia Stuart <georgia.stuart@gmail.com>
2021-02-08 16:32:06 -0600

    Add github link to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'pydata_sphinx_theme'
 
+html_theme_options = {
+  "github_url": "https://github.com/georgiastuart/PythonLPT",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
