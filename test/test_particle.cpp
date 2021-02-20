f4ab50c89908a800b4cbc76ec5c17a9e8cca299d
Georgia Stuart <georgia.stuart@gmail.com>
2021-02-20 15:47:47 -0600

    Add reynolds number tests

--- test/test_particle.cpp
+++ test/test_particle.cpp
@@ -7,10 +7,21 @@
 #include "../src/pythonLPT/cpp/mesh.h"
 
 TEST_CASE("Buoyancy Calculated Correctly", "[buoyancy]") {
-    Particle particle(0.0, 0.0, 0.00005, 858.0, -15.0, 0.023);
-    MeshNode node(998.2071, 10.0, 0.001002);
-
-    SECTION("Eotvos number check") {
-
+    SECTION("Reynold's Numbers") {
+        REQUIRE(Particle::calculate_reynolds(50.0) == Approx(1.72923));
+        REQUIRE(Particle::calculate_reynolds(400.0) == Approx(9.50261));
+        REQUIRE(Particle::calculate_reynolds(1000.0) == Approx(19.0142));
     }
+
+    SECTION("Test configuration 1") {
+        Particle particle(0.0, 0.0, 0.00005, 858.0, -15.0, 0.023);
+        MeshNode node(998.2071, 10.0, 0.001002);
+
+        SECTION("Eotvos number check") {
+
+        }
+    }
+
+
+
 }
