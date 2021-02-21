4de043c0aab7037822439cf9bee2064bcb358e96
Georgia Stuart <georgia.stuart@gmail.com>
2021-02-20 19:45:15 -0600

    Add test for exception throwing in reynolds calc

--- test/test_particle.cpp
+++ test/test_particle.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <catch2/catch.hpp>
+#include <stdexcept>
+#include <iostream>
 #include "../src/pythonLPT/cpp/particle.h"
 #include "../src/pythonLPT/cpp/mesh.h"
 
@@ -11,17 +13,19 @@ TEST_CASE("Buoyancy Calculated Correctly", "[buoyancy]") {
         REQUIRE(Particle::calculate_reynolds(50.0) == Approx(1.72923));
         REQUIRE(Particle::calculate_reynolds(400.0) == Approx(9.50261));
         REQUIRE(Particle::calculate_reynolds(1000.0) == Approx(19.0142));
+        REQUIRE_THROWS_AS(Particle::calculate_reynolds(1.0e8), std::runtime_error);
     }
-
-    SECTION("Test configuration 1") {
-        Particle particle(0.0, 0.0, 0.00005, 858.0, -15.0, 0.023);
-        MeshNode node(998.2071, 10.0, 0.001002);
-
-        SECTION("Eotvos number check") {
-
-        }
-    }
-
-
-
 }
+
+//    SECTION("Test configuration 1") {
+//        Particle particle(0.0, 0.0, 0.00005, 858.0, -15.0, 0.023);
+//        MeshNode node(998.2071, 10.0, 0.001002);
+//
+//        SECTION("Eotvos number check") {
+//
+//        }
+//    }
+
+
+
+//}
