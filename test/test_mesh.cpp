e18ba28f8817a8093c8bdef6a786489389976924
Georgia Stuart <georgia.stuart@gmail.com>
2021-02-22 09:53:42 -0600

    Update fluid viscosity tests

--- test/test_mesh.cpp
+++ test/test_mesh.cpp
@@ -7,8 +7,17 @@
 
 TEST_CASE("Mesh Tests", "[mesh-tests]") {
     SECTION("Water Viscosity Tests") {
+        // Values from Huber et al (2009)
         REQUIRE(MeshNode::calculate_fluid_viscosity(298.15, 998) == Approx(0.000889735100));
         REQUIRE(MeshNode::calculate_fluid_viscosity(298.15, 1200) == Approx(0.001437649467));
         REQUIRE(MeshNode::calculate_fluid_viscosity(373.15, 1000) == Approx(0.000307883622));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(433.15, 1) == Approx(0.000014538324));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(433.15, 1000) == Approx(0.000217685358));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(873.15, 1) == Approx(0.000032619287));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(873.15, 100) == Approx(0.000035802262));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(873.15, 600) == Approx(0.000077430195));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(1173.15, 1) == Approx(0.000044217245));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(1173.15, 100) == Approx(0.000047640433));
+        REQUIRE(MeshNode::calculate_fluid_viscosity(1173.15, 400) == Approx(0.000064154608));
     }
 }
