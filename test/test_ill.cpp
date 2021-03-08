e95555f06530d32b3d82b1eac9544022a4aac5a8
Georgia Stuart <georgia.stuart@gmail.com>
2021-03-08 13:27:10 -0600

    Add insertion to middle of list test

--- test/test_ill.cpp
+++ test/test_ill.cpp
@@ -46,5 +46,14 @@ TEST_CASE("Intrusive Linked List", "[ill]") {
         REQUIRE(p3.node.prev == &p.node);
         REQUIRE(p2.node.next == nullptr);
         REQUIRE(p2.node.prev == nullptr);
+
+        p2.node.insert_after(p.node);
+
+        REQUIRE(p.node.prev == nullptr);
+        REQUIRE(p.node.next == &p2.node);
+        REQUIRE(p2.node.prev == &p.node);
+        REQUIRE(p2.node.next == &p3.node);
+        REQUIRE(p3.node.prev == &p2.node);
+        REQUIRE(p3.node.next == nullptr);
     }
 }
