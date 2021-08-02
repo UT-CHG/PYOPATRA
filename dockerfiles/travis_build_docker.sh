652446a57dbe316976ecce675e7f087ce0146e2b
Georgia Stuart <georgia.stuart@gmail.com>
2021-08-02 08:57:50 -0500

    Fix docker pushing

--- dockerfiles/travis_build_docker.sh
+++ dockerfiles/travis_build_docker.sh
@@ -8,10 +8,21 @@ BUILD_ARRAY=("" "-mvapich-ib" "-sandybridge-mvapich-ib")
 for BUILD_NAME in ${BUILD_ARRAY[@]}; do
   if [[ ${#TRAVIS_TAG} -gt 0 ]];
   then
-    DOCKER_TAG="-t $DOCKER_USERNAME/pyopatra$BUILD_NAME:$TRAVIS_TAG -t $DOCKER_USERNAME/pyopatra$BUILD_NAME:latest"
+#    DOCKER_TAG="-t $DOCKER_USERNAME/pyopatra$BUILD_NAME:$TRAVIS_TAG -t $DOCKER_USERNAME/pyopatra$BUILD_NAME:latest"
+    TAG_ARRAY=("$TRAVIS_TAG" "latest")
   else
-    DOCKER_TAG="-t $DOCKER_USERNAME/pyopatra$BUILD_NAME:unstable"
+    TAG_ARRAY=("unstable")
+#    DOCKER_TAG="-t $DOCKER_USERNAME/pyopatra$BUILD_NAME:unstable"
   fi
-  docker build $DOCKER_TAG -f dockerfiles/Dockerfile"$BUILD_NAME" . \
-  && docker push "$DOCKER_USERNAME"/pyopatra"$BUILD_NAME":"$DOCKER_TAG"
+
+  DOCKER_TAG=""
+  for TAG_NAME in ${TAG_ARRAY[@]}; do
+    DOCKER_TAG="$DOCKER_TAG -t $DOCKER_USERNAME/pyopatra$BUILD_NAME:$TAG_NAME"
+  done
+
+  docker build $DOCKER_TAG -f dockerfiles/Dockerfile"$BUILD_NAME" .
+
+  for TAG_NAME in ${TAG_ARRAY[@]}; do
+    docker push "$DOCKER_USERNAME"/pyopatra"$BUILD_NAME":"$TAG_NAME"
+  done
 done
